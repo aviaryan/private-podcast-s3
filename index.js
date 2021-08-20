c65c99f3b7bb2fb61ce7fc7319d96fd7cb7fee20
Avi Aryan <avi.aryan123@gmail.com>
2021-08-20 17:53:35 +0530

    make feed functional on podcast addict

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Podcast = require("podcast");
 const fs = require("fs");
 
-// thanks to https://www.npmjs.com/package/podcast
+const bucketName = 'bucket-name';
+const rootURL = `https://${bucketName}.s3.amazonaws.com`;
 
 const feed = new Podcast({
   title: "Avi Aryan Personal Podcast",
   description: "This is Avi Aryan Personal Podcast",
-  feed_url: "http://s3.com/rss.xml",
-  site_url: "https://aviaryan.com",
+  feed_url: `${rootURL}/feed.xml`,
+  site_url: rootURL,
   author: "Avi Aryan",
 });
 
@@ -19,7 +20,7 @@ fs.readdirSync('./files/').forEach((file) => {
   feed.addItem({
     title: file,
     description: file,
-    url: "https://aviaryan.com/", // link to the item
+    url: `${rootURL}/files/${file}`, // link to the item
     date: new Date().toISOString(), // any format that js Date can parse.
   });
 });
