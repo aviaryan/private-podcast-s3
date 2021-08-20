a4ebd76ee310fd7b47deb425dd69ed9c03e0862b
Avi Aryan <avi.aryan123@gmail.com>
2021-08-20 17:27:35 +0530

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal Podcast
+
+Thanks to https://www.reddit.com/r/podcasts/comments/74muit/is_it_possible_to_make_an_xml_podcast_feed_from_a/
+
+Another way - https://github.com/nerab/dropcaster
---
+++ index.js
@@ -0,0 +1,11 @@
+import Podcast from "podcast";
+
+
+const feed = new Podcast({
+  title: "Avi Aryan Personal Podcast",
+  description: "This is Avi Aryan Personal Podcast",
+  feed_url: "http://s3.com/rss.xml",
+  site_url: "https://aviaryan.com",
+  author: "Avi Aryan",
+});
+
